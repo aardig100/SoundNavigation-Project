8b2d6fe6164e0e39856a2284725d0ec18d667565
Orobosa Ogbemudia <55265742+aardig100@users.noreply.github.com>
2023-10-13 10:57:55 -0500

    Player Movement initial

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -8,10 +8,11 @@ public class MouseLook : MonoBehaviour
 {
     public InputActionReference horizontalLook;
     public InputActionReference verticalLook;
-    public float lookSpeed = 1f;
+    public float lookSpeed = 0.1f;
     public Transform cameraTransform;
     float pitch;
     float yaw;
+
 
     // Start is called before the first frame update
     void Start()
@@ -29,9 +30,10 @@ public class MouseLook : MonoBehaviour
 
     void HandleVerticalLookChange(InputAction.CallbackContext obj)
     {
-        pitch += obj.ReadValue<float>();
+        pitch -= obj.ReadValue<float>();
         cameraTransform.localRotation = Quaternion.AngleAxis(pitch * lookSpeed, Vector3.right);
     }
+
     // Update is called once per frame
     void Update()
     {
