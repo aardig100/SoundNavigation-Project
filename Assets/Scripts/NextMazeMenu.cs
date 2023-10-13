45cbf8f4b724427150fe1f5f18df995e1a22c083
Sam-Murto <samuel.murto123@gmail.com>
2023-10-13 17:10:43 -0500

    Added next maze screen

---
+++ Assets/Scripts/NextMazeMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.SceneManagement;
+
+public class NextMazeMenu : MonoBehaviour
+{
+    public void GoToNextMaze()
+    {
+        //SceneManager.LoadScene(1 + StateData.currentMaze);
+    }
+}
