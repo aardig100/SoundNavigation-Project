a49709a2f5174939809bb693ac549deafaf1a989
Sam-Murto <samuel.murto123@gmail.com>
2023-10-28 15:49:14 -0500

    Completed data recording

--- Assets/Scripts/PathRecord.cs
+++ Assets/Scripts/PathRecord.cs
@@ -3,25 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using System.IO;
+using UnityEngine.SceneManagement;
 
 public class PathRecord : MonoBehaviour
 {
     float timeElapsed = 0.0f;
     float timeSinceLastPositionUpdate = 0.0f;
     float positionUpdateInterval = 0.5f;
-
+    string path;
     StreamWriter writer;
+    bool recording = true;
+
+    [SerializeField]
+    private CharacterController characterController;
 
     // Start is called before the first frame update
-    void OnEnable()
+    void Awake()
     {
-        string path = Application.persistentDataPath + "/test_data.txt";
+
+        path = Application.persistentDataPath + "/test_data.txt";
+        //Delete file if exists
+        File.Delete(path);
+        //Will create a new file
         writer = new StreamWriter(path, true);
+
+        FlagPickup.OnFlagPickedUp += HandleFlagPickedUp;
+
+        recording = true;
     }
 
     private void OnDisable()
     {
         writer.Close();
+        FlagPickup.OnFlagPickedUp -= HandleFlagPickedUp;
     }
 
     // Update is called once per frame
@@ -30,14 +44,29 @@ public class PathRecord : MonoBehaviour
         timeElapsed += Time.deltaTime;
         timeSinceLastPositionUpdate += Time.deltaTime;
 
-        if (timeSinceLastPositionUpdate > positionUpdateInterval)
+        if (timeSinceLastPositionUpdate > positionUpdateInterval && recording)
         {
             //Record position and elapsed time into file
-            writer.WriteLine("Testing input: " + timeElapsed + " seconds have passed");
-
+            writer.WriteLine(characterController.transform.position.x + "," + characterController.transform.position.z +  "," + timeElapsed);
+            Debug.Log("Writing line to " + path);
             timeSinceLastPositionUpdate = 0;
         }
         
+
+
+    }
+
+    private void HandleFlagPickedUp()
+    {
+
+        if (recording)
+        {
+            recording = false;
+            writer.WriteLine(timeElapsed);
+
+            SceneManager.LoadScene(3);
+        }
+
 
 
     }
